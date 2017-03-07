31e6dabc3ce8d3f786dd1d3634958d6419ffe7af
Burgess <king351246989@gmail.com>
2017-03-07 20:06:35 +0800

    Add files via upload

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 from socket import *
 import time
+import sys
+import os
 
 HOST = 'localhost'
-PORT = 1111
+PORT = 9500
 BUFIZ = 1024
 ADDR = (HOST,PORT)
 
@@ -32,24 +34,46 @@ def sendfile(filename):
     time.sleep(1)
     cliSockfd.send('EOF')
     print 'send file success!'
+def handle1(act,filename):
+    if act == 'put':
+        print 'recving msg!'
+        recvfile(filename)
+    elif act == 'get':
+        print 'sending msg!'
+        sendfile(filename)
+    else:
+        print 'error!'
+
+def handle2(act):
+    if act == 'ls':
+        path = sys.path[0]
+        every_file = os.listdir(path)
+        for filename in every_file: 
+            cliSockfd.send(filename + '  ')
+        time.sleep(1)
+        cliSockfd.send('EOF')
+        print 'all filename has send to client success!'
+    else:
+        print 'command error'
 
 sockfd = socket(AF_INET,SOCK_STREAM)
 sockfd.bind(ADDR)
 sockfd.listen(5)
 while True:
-    print('waiting for connection...')
+    print 'waiting for connection...'
     cliSockfd,addr = sockfd.accept()
-    print('...connected from:',addr)
+    print '...connected from:',addr
     
     while True:
         msg = cliSockfd.recv(4096)
-        act,filename = msg.split()
-        if act == 'put':
-            print 'recving msg!'
-            recvfile(filename)
-        elif act == 'get':
-            print 'sending msg!'
-            sendfile(filename)
+        if msg == 'close':
+            print 'client closed'
+            break
+        info = msg.split()
+        if len(info) == 2:
+            handle1(*info)
+        elif len(info) == 1:
+            handle2(*info)
         else:
-            print 'error!'
-            continue
+            print 'command error!'
+            break
